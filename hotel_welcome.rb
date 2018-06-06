f1b4b64c9f2e679a9c604602b6535b474c45da5e
Natalia0727 <oicafn@gmail.com>
2018-06-06 17:34:00 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,9 @@
+def tv_welcome(name,room_number)
+  
+  puts "Welcome #{name}".upcase
+  puts "to the Springhill suites Pheonix Downtown"
+  puts "Room #{room_number}"
+  
+end 
+
+tv_welcome("NATALIA","234")
---
+++ loops.rb
@@ -0,0 +1,34 @@
+# # #10.times do 
+# #   #puts"where is my lunch "
+# # #end
+# # loop do
+# #   puts "grrruuuuummmble grrrruuuuuummmble grrrrrruuuuummmmble"
+# #   puts "im so hungry"
+# #   puts ":("
+# # end
+# def dance
+#   moves_slayed=0
+#   loop do 
+#     if moves_slayed==10
+#       break 
+#     end
+#   puts "step to the left"
+#   puts "step to the right"
+#   puts "kick"
+#   moves_slayed=moves_slayed+1
+# end
+# end
+# dance
+
+def kwk_camp
+  days=11
+  loop do 
+    if days==0
+    puts "camp is over!"
+      break
+    end 
+    days=days-1
+    puts "there are #{days} days left"
+  end 
+end
+kwk_camp
